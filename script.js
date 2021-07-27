1855edc32f50b337ac805d76ac51830e882eb5ac
dipen2000 <dipenchavda2000@gmail.com>
2021-07-28 01:12:22 +0530

    Finished : Talkative bot

---
+++ script.js
@@ -0,0 +1,53 @@
+const introBtn = document.getElementById("btn");
+const textArea = document.getElementById("txtarea");
+const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
+const stopBtn = document.getElementById("stop");
+const resumeBtn = document.getElementById("resume");
+let currentChar;
+//Some global properties related to utterance which are going to remian same for both the buttons
+const guestName = prompt("Can I know your name?");
+const utterance = new SpeechSynthesisUtterance();
+utterance.rate = 1;
+utterance.addEventListener("end" , ()=>{
+    
+    playBtn.classList.add("active")
+    resumeBtn.classList.remove("active");    
+    textArea.disabled = false;    
+})
+
+playBtn.addEventListener("click",()=>{
+    const textFromArea = textArea.value;
+    console.log(speechSynthesis.paused);
+    console.log(speechSynthesis.speaking);
+    if(speechSynthesis.speaking){
+        return;
+    }  
+    textArea.disabled = true;
+    utterance.text = textFromArea;
+    speechSynthesis.speak(utterance);
+});
+
+pauseBtn.addEventListener("click" , ()=>{
+    playBtn.classList.remove("active")
+    resumeBtn.classList.add("active");
+    speechSynthesis.pause();
+});
+
+stopBtn.addEventListener("click" , ()=>{
+    speechSynthesis.cancel();
+});
+
+resumeBtn.addEventListener('click' , ()=>{
+    speechSynthesis.resume();
+});
+
+introBtn.addEventListener("click",()=>{
+    if(speechSynthesis.speaking){
+        return;
+    }
+    utterance.text = `Hello ${guestName}, I welcome you on behalf of Deepan.`
+    speechSynthesis.speak(utterance);
+});
+
+
